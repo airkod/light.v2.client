c317c5334df03f71618ad664994e850ce9f300cc
Ed Kom <execrot@airkod.com>
2023-11-27 08:44:50 +0200

    update

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,7 +3,6 @@ import { Body } from "./interfaces/body";
 export declare class LightV2Client {
     private config;
     private session;
-    build: Builder;
     constructor(config: Config);
     private auth;
     private isValidAccessToken;
@@ -22,29 +21,4 @@ export declare class LightV2Client {
     deleteOne(collection: string, cond: any): Promise<void>;
     deleteMany(collection: string, cond: any): Promise<void>;
     command<T>(collection: string, command: any): Promise<T>;
-}
-export declare class Builder {
-    private client;
-    private workspace;
-    constructor(client: LightV2Client);
-    collection(collection: string): Builder;
-    cond(cond: any): Builder;
-    sort(sort: any): Builder;
-    limit(limit: number): Builder;
-    skip(skip: number): Builder;
-    data(data: any): Builder;
-    command(command: any): Builder;
-    all<T>(): Promise<{
-        count: number;
-        data: Array<T>;
-    }>;
-    one<T>(): Promise<T>;
-    count(): Promise<number>;
-    insertOne(): Promise<void>;
-    insertMany(): Promise<void>;
-    updateOne(): Promise<void>;
-    updateMany(): Promise<void>;
-    deleteOne(): Promise<void>;
-    deleteMany(): Promise<void>;
-    cmd<T>(): Promise<T>;
 }
