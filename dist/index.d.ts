977c02c6153ce5ff02ae571ff74e82b84b460d85
Ed Kom <execrot@airkod.com>
2023-11-27 08:47:49 +0200

    update

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -21,4 +21,34 @@ export declare class LightV2Client {
     deleteOne(collection: string, cond: any): Promise<void>;
     deleteMany(collection: string, cond: any): Promise<void>;
     command<T>(collection: string, command: any): Promise<T>;
+    build(): Builder;
+    sample(): SampleClass;
+}
+export declare class SampleClass {
+    field: number;
+}
+export declare class Builder {
+    private client;
+    private workspace;
+    constructor(client: LightV2Client);
+    collection(collection: string): Builder;
+    cond(cond: any): Builder;
+    sort(sort: any): Builder;
+    limit(limit: number): Builder;
+    skip(skip: number): Builder;
+    data(data: any): Builder;
+    command(command: any): Builder;
+    all<T>(): Promise<{
+        count: number;
+        data: Array<T>;
+    }>;
+    one<T>(): Promise<T>;
+    count(): Promise<number>;
+    insertOne(): Promise<void>;
+    insertMany(): Promise<void>;
+    updateOne(): Promise<void>;
+    updateMany(): Promise<void>;
+    deleteOne(): Promise<void>;
+    deleteMany(): Promise<void>;
+    cmd<T>(): Promise<T>;
 }
